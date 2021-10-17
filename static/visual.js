f25a3609860f829436be388226a9c88ee104aadb
Arshad <ashsams@hotmail.com>
2021-10-17 10:32:24 +0100

    Js update

--- static/visual.js
+++ static/visual.js
@@ -69,4 +69,55 @@ function buildPlot2(countryIndex) {
   });
 }
 
+function buildPlot3(countryIndex) {
+  const url = "/temp";
+  d3.json(url).then(function (data) {
+    countries = []
+    Y1963 = []
+    Y1973 = []
+    Y1983 = []
+    Y1993 = []
+    Y2003 = []
+    Y2013 = []
+    data.forEach(function (element) {
+      countries.push(element.Country)
+      Y1963.push(element.Y1963)
+      Y1973.push(element.Y1973)
+      Y1983.push(element.Y1983)
+      Y1993.push(element.Y1993)
+      Y2003.push(element.Y2003)
+      Y2013.push(element.Y2013)
+      // console.log(countries[0])
+    });
+
+
+    countryName = countries[countryIndex];
+    let singleY1963 = Y1963[countryIndex];
+    let singleY1973 = Y1973[countryIndex];
+    let singleY1983 = Y1983[countryIndex];
+    let singleY1993 = Y1993[countryIndex];
+    let singleY2003 = Y2003[countryIndex];
+    let singleY2013 = Y2013[countryIndex];
+
+    x_data = ["Y1973", "Y1983", "Y1993", "Y2003", "Y2013"];
+    y_data = [singleY1973, singleY1983, singleY1993, singleY2003, singleY2013];
+
+    console.log(countryName);
+    console.log(x_data);
+    console.log(y_data);
+    let trace1 = {
+      x: x_data,
+      y: y_data,
+      type: "bar",
+      name: "bar"
+    };
+    var data = [trace1];
+    var layout = {
+      title: countryName + " Temperature over Years",
+      showlegend: false
+    }
+
+    Plotly.newPlot("plottemp", data, layout);
+  });
+}
 updateDashboard(countryIndex = -1);
